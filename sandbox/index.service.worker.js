7f25be8d9cbc568127500719077cf05a7f4f0132
sina.shams@yahoo.com <sina.shams@yahoo.com>
2025-03-13 18:41:21 +0330

    Update game files

--- sandbox/index.service.worker.js
+++ sandbox/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1741878264|1622692330';
+const CACHE_VERSION = '1741878677|2035464143';
 /** @type {string} */
 const CACHE_PREFIX = 'Sand Box-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
